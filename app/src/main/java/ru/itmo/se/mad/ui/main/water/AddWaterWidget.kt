fe816f1fdd2ed01492818b3676b1978512db4efb
Pietyboy <danlukinskiy@gmail,com>
2025-05-05 17:28:04 +0300

    Fix widget's design

--- app/src/main/java/ru/itmo/se/mad/ui/main/water/AddWaterWidget.kt
+++ app/src/main/java/ru/itmo/se/mad/ui/main/water/AddWaterWidget.kt
@@ -1,5 +1,6 @@
 package ru.itmo.se.mad.ui.main.water
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,8 +27,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.itmo.se.mad.ui.theme.WaterTrackerTheme
 import androidx.compose.foundation.clickable
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import ru.itmo.se.mad.R
 import ru.itmo.se.mad.ui.theme.SFProDisplay
+import ru.itmo.se.mad.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -131,39 +136,43 @@ fun WaterCard(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(color = Color(0xFF00AFFF), shape = RoundedCornerShape(20.dp))
+                .background(color = Color(0xFF00A6FF), shape = RoundedCornerShape(20.dp))
                 .padding(16.dp)
         ) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = "%.2f".format(totalWater),
-                    fontSize = 28.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = SFProDisplay
+            Text(
+                text = buildAnnotatedString {
+                    withStyle(style = SpanStyle(color = Color.White)) {
+                        append("%.1f".format(totalWater))
+                    }
+                    withStyle(style = SpanStyle(color = Color(0xB3FFFFFF))) {
+                        append("/%.2f л".format(maxWater))
+                    }
+                },
+                fontSize = 34.sp,
+                fontWeight = FontWeight.SemiBold,
+                fontFamily = SFProDisplay,
                 )
-                Text("/${maxWater} л",
-                    fontSize = 28.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = SFProDisplay
+
+            Text(
+                "В том числе\n из еды: 0.3 л",
+                fontSize = 14.sp,
+                color = Color.White,
+                fontWeight = FontWeight.Normal,
+                fontFamily = SFProDisplay,
+                lineHeight = 18.sp,
+                textAlign = TextAlign.End,
                 )
-                Text("В том числе\n из еды: 0.3 л",
-                    fontSize = 14.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Normal,
-                    fontFamily = SFProDisplay
-                )
-            }
+        }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
+                horizontalArrangement = Arrangement.Start
             ) {
                 WaterItem(
                     label = "Стакан",
@@ -194,10 +203,23 @@ fun WaterCard(
                 modifier = Modifier
                     .fillMaxWidth()
                     .clickable { /* Add custom volume */ },
-                horizontalArrangement = Arrangement.Start
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                Spacer(modifier = Modifier.width(8.dp))
-                Text("+ Другой объём",
+                Box(
+                    modifier = Modifier
+                        .size(38.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.baseline_add_circle_24),
+                        contentDescription = "Add",
+                        tint = Color.White,
+                        modifier = Modifier.size(16.dp)
+                    )
+                }
+                Spacer(modifier = Modifier.width(15.dp))
+                Text("Другой объём",
                     fontSize = 16.sp,
                     color = Color.White,
                     fontFamily = SFProDisplay
@@ -217,34 +239,43 @@ fun WaterCard(
 
 @Composable
 fun WaterItem(label: String, volume: String, onClick: () -> Unit) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
         Box(
             modifier = Modifier
-                .size(40.dp)
+                .size(38.dp)
                 .clickable(onClick = onClick)
-                .background(Color(0x66000000), shape = RoundedCornerShape(20.dp)),
+                .background(Color(0x26FFFFFF), shape = RoundedCornerShape(20.dp)),
             contentAlignment = Alignment.Center
         ) {
             Icon(
                 painter = painterResource(id = R.drawable.baseline_add_circle_24),
                 contentDescription = "Add",
                 tint = Color.White,
-                modifier = Modifier.size(24.dp)
+                modifier = Modifier.size(16.dp)
             )
         }
-        Spacer(modifier = Modifier.height(4.dp))
-        Text(label,
-            color = Color.White,
-            fontFamily = SFProDisplay
+        Spacer(modifier = Modifier.width(15.dp))
+        Text(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.White)) {
+                    append(label)
+                }
+                append("\n")
+                withStyle(style = SpanStyle(color = Color(0xB3FFFFFF), fontSize = 14.sp)) {
+                    append(volume)
+                }
+            },
+            lineHeight = 16.sp,
+            fontSize = 16.sp,
+            fontFamily = SFProDisplay,
         )
-        Text(volume,
-            color = Color.White.copy(alpha = 0.8f),
-            fontSize = 12.sp,
-            fontFamily = SFProDisplay
-        )
+        Spacer(modifier = Modifier.width(20.dp))
     }
 }
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WaterSlider(
@@ -294,7 +325,7 @@ fun WaterSlider(
                 modifier = Modifier
                     .fillMaxWidth(fillWidth / componentWidth)
                     .height(80.dp)
-                    .clip(RoundedCornerShape(24.dp))
+                    .clip(RoundedCornerShape(24.dp,  0.dp, 0.dp, 24.dp))
                     .background(Color(0xFF66CCFF))
             )
 
@@ -338,7 +369,7 @@ fun WaterSlider(
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Box(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier.weight(0.9f),
                         contentAlignment = Alignment.CenterStart
                     ) {
                         if (dragging) {
@@ -359,7 +390,7 @@ fun WaterSlider(
                     }
 
                     Box(
-                        modifier = Modifier.weight(1f),
+                        modifier = Modifier.weight(1.1f),
                         contentAlignment = Alignment.CenterEnd
                     ) {
                         if (dragging) {
@@ -368,10 +399,16 @@ fun WaterSlider(
                                 modifier = Modifier.fillMaxWidth()
                             ) {
                                 Text(
-                                    text = "+%.2f л".format(addedWater.value),
-                                    fontSize = 24.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color.White,
+                                    text = buildAnnotatedString {
+                                        withStyle(style = SpanStyle(color = Color(0xB3FFFFFF))) {
+                                            append("+ ")
+                                        }
+                                        withStyle(style = SpanStyle(color = White)) {
+                                            append("%.2f л".format(addedWater.value))
+                                        }
+                                    },
+                                    fontSize = 34.sp,
+                                    fontWeight = FontWeight.SemiBold,
                                     modifier = Modifier.fillMaxWidth(),
                                     textAlign = TextAlign.End,
                                     fontFamily = SFProDisplay
@@ -379,7 +416,7 @@ fun WaterSlider(
                                 )
                                 Text(
                                     text = "Отпустите, чтобы добавить",
-                                    fontSize = 14.sp,
+                                    fontSize = 16.sp,
                                     color = Color.White,
                                     modifier = Modifier.fillMaxWidth(),
                                     lineHeight = 16.sp,
@@ -389,20 +426,28 @@ fun WaterSlider(
                                 )
                             }
                         } else {
-                            Row(verticalAlignment = Alignment.CenterVertically) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically) {
                                 Icon(
                                     painter = painterResource(id = R.drawable.baseline_arrow_forward_ios_24),
                                     contentDescription = null,
                                     tint = Color.White,
-                                    modifier = Modifier.size(16.dp),
+                                    modifier = Modifier.size(24.dp),
 
                                 )
-                                Spacer(modifier = Modifier.width(4.dp))
                                 Text(
-                                    text = "%.1f/%.2f л".format(totalWater, maxWater),
-                                    fontSize = 24.sp,
-                                    fontWeight = FontWeight.Bold,
-                                    color = Color.White,
+                                    text = buildAnnotatedString {
+                                        withStyle(style = SpanStyle(color = Color.White)) {
+                                            append("%.1f".format(totalWater))
+                                        }
+                                        withStyle(style = SpanStyle(color = Color(0xB3FFFFFF))) {
+                                            append("/%.2f л".format(maxWater))
+                                        }
+                                    },
+                                    fontSize = 34.sp,
+                                    fontWeight = FontWeight.SemiBold,
                                     fontFamily = SFProDisplay
 
                                 )
